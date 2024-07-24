5beceeda2de7e125a23b2b94256a4570e571baff
ibrahimmohamed990 <ibrahimmohamed990>
2024-07-24 22:21:41 +0300

    update

--- Store.Repository/obj/Debug/net8.0/Store.Repository.AssemblyInfo.cs
+++ Store.Repository/obj/Debug/net8.0/Store.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96e50cfd69431221829344ae66a28805447e5fd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
